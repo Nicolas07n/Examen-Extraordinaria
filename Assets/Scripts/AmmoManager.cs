869c5095134f886b3c02e0639e859d4d241c1e65
Alvergel25 <nicolasmg4012@gmail.com>
2025-06-16 17:48:56 +0200

    Game Manager
    Game Manager

--- Assets/Scripts/AmmoManager.cs
+++ Assets/Scripts/AmmoManager.cs
@@ -1,11 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AmmoManager
 {
     public static AmmoManager instance;
+    private int numeroPelotas = 0;
+    public int NumeroPelotas=>numeroPelotas;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            GraphElementScopeExtensions
+        }
+   
+        else
+        {
+            Destroy(gameObject);
+
+
+        }
+    
+    
+    
+    
+    }  
+
+
+    public void RegistrarNumeroPelotas()
+    {
+
+
+        numeroPelotas++;
+    }
+
+
+
+
 
     private AmmoManager()
     {
