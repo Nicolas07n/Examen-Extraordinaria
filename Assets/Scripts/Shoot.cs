fd293363572e40f5d01ff667f2e77b7abd8699b4
Alvergel25 <nicolasmg4012@gmail.com>
2025-06-16 17:35:52 +0200

    ShootBullet
    ShootBullet

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
 
-    private void Start()
-    {
+    public GameObject bulletPrefab;
+    public Transform firepoint;//LO que hago con el fire point es crear un empty(Objeto vacio) hijo del personaje para que lo siga , y que sea desde ahi donde se instancia la bala 
+    public float fireRate = 0.3f;
+    private float nextFireTime;
 
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime) 
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
         
+        
+        
+        }
+
+
     }
+
+    void Shoot()
+    {
+
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, Quaternion.identity);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - firepoint.position; 
+        bullet.GetComponent<Bullet>().SetDirection(direction);
+    }
+
 }
