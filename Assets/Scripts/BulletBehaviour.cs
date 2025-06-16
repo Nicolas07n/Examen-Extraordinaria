b54e6a308f04667b2eb7a38fdaf4ce726f1c5a12
Alvergel25 <nicolasmg4012@gmail.com>
2025-06-16 17:13:35 +0200

    bullet creada

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -1,29 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
     // public Bullet bullet;
-    public Vector2 dir;
-
+    public Vector2 direccion;
+    public float speed = 1.0f;  
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-
+    private int bouncecount = 0;
+    private int maxbcecount = 0;
     // Start is called before the first frame update
     void Start()
     {    
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        
+       rb.velocity = Direction.normalized*speed;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public void SetDirction(Vector2 dir)
     {
+        Direction = dir.normalized;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+
+            return;
+            bouncecount++;
+        }
+        if (bouncecount == maxbcecount) 
+        {
+
+            Destroy(gameObject);
+        
+        
+        }
+   
+    
     }
+
+
+
+
 }
