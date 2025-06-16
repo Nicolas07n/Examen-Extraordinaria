aeaf3a3da78e485528ed3e61edce8d50e8bb45e1
Alvergel25 <nicolasmg4012@gmail.com>
2025-06-16 16:54:28 +0200

    Canvas
    canvas

--- Assets/Scripts/ShowNextBullet.cs
+++ Assets/Scripts/ShowNextBullet.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShowNextBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public TextMeshProUGUI textoCambios;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        if (textoCambios != null)
+        {
+            textoCambios.Cambios.text = "Siguiente bala:" + GameManager.instance.CambiosDireccion;
+        }
+    
+         
+    
     }
 }
